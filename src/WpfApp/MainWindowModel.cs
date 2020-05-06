9d37b0a0b146be1a320c13cd4b1bc4f1c69a025c
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2020-05-06 15:04:05 +0200

    added template

---
+++ src/WpfApp/MainWindowModel.cs
@@ -0,0 +1,15 @@
+﻿using Plc;
+
+namespace WpfApp
+{
+    public class MainWindowModel
+    {
+        public MainWindowModel()
+        {
+            Entry.Plc.Connector.BuildAndStart();
+            MAIN = Entry.Plc.MAIN;
+        }
+
+        public MAIN MAIN { get; set; }
+    }
+}
